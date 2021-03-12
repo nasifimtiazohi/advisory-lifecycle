d668426bf6bec396e281f71cb30155535c5921d1
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-03-12 13:16:28 -0500

    changing database to Mar5 one

--- data_explore/sql.py
+++ data_explore/sql.py
@@ -5,7 +5,7 @@ import pandas as pd
 import csv
 import os
 import datetime
-database='snykvuln'
+database='snykvulnMar5'
 import sqlalchemy as db
 engine = db.create_engine('mysql+pymysql://root:@localhost:3306/{}'.format(database))
 
